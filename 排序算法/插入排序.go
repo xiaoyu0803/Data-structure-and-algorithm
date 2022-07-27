904757d0e98bb46efd2524f5a25d9e704261081c
xiaoyu0803 <442074447@qq.com>
2022-07-27 18:04:27 +0800

    ...

---
+++ 排序算法/插入排序.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+func insertSort(arr []int) []int {
+	for i := range arr {
+		preIndex := i - 1
+		current := arr[i]
+		for preIndex >= 0 && arr[preIndex] > current {
+			arr[preIndex+1] = arr[preIndex]
+			preIndex -= 1
+		}
+		arr[preIndex+1] = current
+	}
+	return arr
+}
